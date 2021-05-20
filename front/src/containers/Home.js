b9c89b78d4a73fb6e6c77a7118d0d237ab0a81be
oscar-info <oscar.info@vecindario.com>
2021-05-20 11:40:31 -0500

    componente card projects creado junto con sus estilos tambien se agregan las imagenes necesarias para este componente

--- front/src/containers/Home.js
+++ front/src/containers/Home.js
@@ -2,13 +2,15 @@ import React from 'react'
 import "../assets/styles/components/Home.scss"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-// import CardProject from "../components/CardProject"
+import CardProject from "../components/CardProject"
 
 function Home(){
     return (
         <div className="home">
             <Header/>
-            <div className="container__projects"></div>
+            <div className="container__projects">
+                <CardProject/>
+            </div>
             <Footer/>
         </div>
     )
