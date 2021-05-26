638524e88f5ef778bf3bc0503ee22081bc2aa82c
oscar-info <oscar.info@vecindario.com>
2021-05-26 10:28:16 -0500

    method name change :authorize_request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
+
+    rescue_from StandardError do |e|
+        render json: {error: e.message}, status: :internal_error
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |e|
+        render json: {error: e.message}, status: :unprocessable_entity
+    end
+
     def not_found
         render json: { error: 'not_found' }
     end
 
-    def authorize_resquest
+    def authorize_request
         header = request.headers['Authorization']
         header = header.split(' ').last if header
         begin
