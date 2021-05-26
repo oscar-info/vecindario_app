7ad87a37143c712612f4b6d95d77d3efa8d49ede
oscar-info <oscar.info@vecindario.com>
2021-05-26 10:57:52 -0500

    generating migrations to change the type of the phone column of users and leads

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_220738) do
+ActiveRecord::Schema.define(version: 2021_05_26_155254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_05_22_220738) do
     t.string "name"
     t.string "last_name"
     t.string "email"
-    t.integer "phone"
+    t.string "phone"
     t.bigint "project_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2021_05_22_220738) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "last_name"
-    t.integer "phone"
+    t.string "phone"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
