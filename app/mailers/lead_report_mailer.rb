3c32c07913ce531b88972a95f21232e198086031
oscar-info <oscar.info@vecindario.com>
2021-06-08 08:18:11 -0500

    spelling error correction in subject

--- app/mailers/lead_report_mailer.rb
+++ app/mailers/lead_report_mailer.rb
@@ -3,6 +3,6 @@ class LeadReportMailer < ApplicationMailer
     def lead_report(lead, emails, name_project)
         @lead = lead
         @name_project = name_project
-        mail to: emails, subject: "Hay usuario interezado en tu proyecto!"
+        mail to: emails, subject: "Hay usuario interesado en tu proyecto!"
     end
 end
